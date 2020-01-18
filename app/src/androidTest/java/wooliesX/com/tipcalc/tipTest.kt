786331e7d6f859a116774e35a5853b3a0c205f98
sandyatww <55424320+sandyatww@users.noreply.github.com>
2020-01-18 20:23:57 +1100

    added support for app center

--- app/src/androidTest/java/wooliesX/com/tipcalc/tipTest.kt
+++ app/src/androidTest/java/wooliesX/com/tipcalc/tipTest.kt
@@ -1,15 +1,18 @@
 package wooliesX.com.tipcalc
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.microsoft.appcenter.espresso.Factory
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
-import androidx.test.ext.junit.runners.AndroidJUnit4
 
 
 @RunWith(AndroidJUnit4::class)
@@ -17,6 +20,7 @@ class tipTest {
 
     @Rule @JvmField
     var activityActivityTestRule = ActivityTestRule(MainActivity::class.java)
+    var reportHelper = Factory.getReportHelper()
 
 
     @Test
@@ -27,6 +31,9 @@ class tipTest {
         onView(withId(R.id.infoTextView)).check(matches(withText("Tip: $4.00 Total: $44.00")))
     }
 
-
+    @After
+    fun TearDown() {
+        reportHelper.label("Stopping App")
+    }
 
 }
--- app/src/main/java/wooliesX/com/tipcalc/MainActivity.kt
+++ app/src/main/java/wooliesX/com/tipcalc/MainActivity.kt
@@ -1,14 +1,22 @@
 package wooliesX.com.tipcalc
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
+//import sun.jvm.hotspot.utilities.IntArray
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCenter.start(
+            application, "b7e9d021-df5d-4131-95c9-4184f16ecadf",
+            Analytics::class.java, Crashes::class.java
+        )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         infoTextView.visibility = View.INVISIBLE
