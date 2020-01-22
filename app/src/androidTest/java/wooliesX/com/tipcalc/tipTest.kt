f704f5ba19c413232bd2d9c53b246f0df8b12da0
sandyatww <55424320+sandyatww@users.noreply.github.com>
2020-01-22 11:57:40 +1100

    added  e2e test

--- app/src/androidTest/java/wooliesX/com/tipcalc/tipTest.kt
+++ app/src/androidTest/java/wooliesX/com/tipcalc/tipTest.kt
@@ -8,7 +8,7 @@ import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.microsoft.appcenter.espresso.Factory
+
 import org.junit.After
 import org.junit.Rule
 import org.junit.Test
@@ -18,22 +18,28 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class tipTest {
 
+
     @Rule @JvmField
     var activityActivityTestRule = ActivityTestRule(MainActivity::class.java)
-    var reportHelper = Factory.getReportHelper()
-
 
     @Test
-    fun validateTip(){
+    fun Step01_enterBillAmount(){
         onView(withId(R.id.billEditText)).perform(typeText("40"))
-        onView(withId(R.id.tipPercentageEditText)).perform(typeText("10"))
-        onView(withId(R.id.calculateButton)).perform(click())
-        onView(withId(R.id.infoTextView)).check(matches(withText("Tip: $4.00 Total: $44.00")))
     }
 
-    @After
-    fun TearDown() {
-        reportHelper.label("Stopping App")
+    @Test
+    fun Step02_enterTipPercentage(){
+        onView(withId(R.id.tipPercentageEditText)).perform(typeText("10"))
+    }
+
+    @Test
+    fun Step03_calculateTip(){
+        onView(withId(R.id.calculateButton)).perform(click())
+    }
+
+    @Test
+    fun Step04_validateCalculatedTip(){
+        onView(withId(R.id.infoTextView)).check(matches(withText("Tip: $4.00 Total: $44.00")))
     }
 
 }
